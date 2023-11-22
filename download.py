a4e2618d2c7e399e9587bf7fa757c3cebccc64ef
aten88 <virtuoso_ten@mail.ru>
2023-11-22 16:43:46 +0300

    Сделать 5 урок

---
+++ download.py
@@ -0,0 +1,29 @@
+import requests_cache
+from bs4 import BeautifulSoup
+import re
+from urllib.parse import urljoin
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent
+
+DOWNLOADS_URL = 'https://docs.python.org/3/download.html'
+
+if __name__ == '__main__':
+    session = requests_cache.CachedSession()
+    response = session.get(DOWNLOADS_URL)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'lxml')
+    main_tag = soup.find('div', {'role': 'main'})
+    table_tag = main_tag.find('table', {'class': 'docutils'})
+    pdf_a4_tag = table_tag.find('a', {'href': re.compile(r'.+pdf-a4\.zip$')})
+    pdf_a4_link = pdf_a4_tag['href']
+    archive_url = urljoin(DOWNLOADS_URL, pdf_a4_link)
+    filename = archive_url.split('/')[-1]
+
+    downloads_dir = BASE_DIR / 'downloads'
+    downloads_dir.mkdir(exist_ok=True)
+    archive_path = downloads_dir / filename
+    response = session.get(archive_url)
+    with open(archive_path, 'wb') as file:
+        file.write(response.content)
