754a4223e981168223d8cc6e80859412180d1a3a
aten88 <virtuoso_ten@mail.ru>
2023-12-08 12:19:21 +0300

    Сделал парсинг динамич страниц

---
+++ dynamic_parsing.py
@@ -0,0 +1,12 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+
+if __name__ == '__main__':
+
+    session = HTMLSession()
+    response = session.get('https://httpbin.org/')
+    response.html.render(sleep=3)
+    soup = BeautifulSoup(response.html.html, 'lxml')
+    swagger = soup.find(id='swagger-ui')
+    print(swagger.prettify())
